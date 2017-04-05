2fdc3d617c02d79b1bf5932a5abfc00beed66c3e
Mathieu Barbe <Mathieu Barbe>
2017-04-05 23:36:48 +0200

    ajout des __repr

--- bentley.py
+++ bentley.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import os
+
+from segment import Point, Cross, Event, Segment, CROSS, START, END
+
+seg1 = Segment(0,0,0,1)
+print("seg1 :")
+print(seg1)
+seg2 = Segment(10,11,20,21)
+
+cr1 = Cross(5, 5, [seg1, seg2])
+print("cr1 :")
+print(cr1)
+
+ev1 = Event(1,1, CROSS, cr1)
+print("ev1 :")
+print(ev1)
--- segment.py
+++ segment.py
@@ -3,6 +3,13 @@
 
 from math import atan, pi
 
+# Constantes
+# Type d'événement 
+# Triés par ordre de priorité, 0 à traiter en premier.
+CROSS = 0
+START = 1
+END = 2
+
 class Point(object):
 	def __init__(self, x, y):
 		self.x = x
@@ -14,29 +21,63 @@ class Point(object):
 	# end def
 # end class
 
-class Croisement(Point):
+class Cross(Point):
 	"""Point avec les segments associé"""
-	def __init__(self, x, y):
-		self.segments = []
+	def __init__(self, x, y, list_seg):
+		if len(list_seg) < 2:
+			raise IOErorr("A cross must be contain at least 2 segments")
+		# end if
+		self.segs = list_seg
 		return Point.__init__(self, x, y)
+	# end def
+	
+	def __repr__(self):
+		txt = "Cross at %.4f : %.4f\nSegments associated :" %(self.x, self.y)
+		for seg in self.segs:
+			txt += "\n" + Segment.__repr__(seg)
+		return txt
 	# end def
 # end class
 
-class Evenement(Point):
-	def __gt__(self, eve2):
-		"""Trié par X puis par inverse Y"""
-		# comparaison par abcisse si ordonné identique
-		if self.y == eve2.y:
-			if self.x == eve2.x :
-				# Les deux événements sont au même point
-				# Il faut gérer ici l'ordre si il sont de type diférent
-				return False # temporaire
-			# end if
-			return self.x < eve2.x
+class Event(Point):
+	def __init__(self, x, y, type, event):
+		if type not in [CROSS, START, END]: 
+			raise IOError("Event must be associated with a type : CROSS, START or END")
 		# end if
-		else:
-			return self.y > eve2.y
-		# end else
+		self.type = type
+		self.p_event = event
+		return Point.__init__(self, x, y)
+	# end def
+	
+	def __repr__(self):
+		type_name = self.fetch_type_name()
+		return "Event type : " + type_name +"\n" + self.p_event.__repr__() + "\n"
+	# end def
+	
+	def fetch_type_name(self):
+		if self.type == CROSS : return "CROSS"
+		if self.type == START : return "START"
+		if self.type == END : return "END"
+		return "Error type"
+	# end def
+	
+	def __gt__(self, eve2):
+		""" Trié par (du plus petit au plus grand :
+		y
+		si y identique, inverse de x
+		si x et y identique, on a :
+		croisement < debut < fin
+		Les plus petit événement sont exploités en premier"""
+		
+		if self.y > eve2.y: return True
+		if self.y < eve2.y : return False
+		# Si on est encore là, l'ordonné est identique.
+		if self.x < eve2.x : return True
+		if self.x > eve2.x : return False
+		# Si on est encore là, abcisse et ordonné identique
+		if self.type > eve2.type : return True
+		# Si on est encore là, les deux points on exactement la même priorité.
+		return False
 	# end def
 # end class
 
@@ -62,14 +103,3 @@ class Segment(object):
 		return "Start : %s\nEnd : %s\nAngle : %.4f radians" %(self.start, self.end, self.angle)
 		# end def 
 # end class
-
-
-		
-seg = Segment(0,0,0,1)
-print(seg)
-
-eve1 = Evenement(1,1)
-eve2 = Evenement(2, 2)
-print(eve1)
-print(eve2)
-print(eve1 > eve2)
