fb3b764787f7ac0665d34f54e98e2a683b215e20
Mathieu Barbe <mat.barbe@gmail.com>
2017-04-09 23:05:46 +0200

    mise en place des events

--- bentley.py
+++ bentley.py
@@ -11,12 +11,29 @@ def bentley(filename):
     """
     run bentley ottmann
     """
+    
+    # Global_y représente l'ordonné de l'évennement en cours d'étude.
+    global g_y
+    
     adjuster, segments = load_segments(filename)
-    events = init(segments)
-    for eve in events :
+    events = init_events(segments)
+    # Création de la liste des segments actifs triés par abscisse.
+    actifs_segments = []
+    # Création de la liste des croisements.
+    cross_list = []
+    
+    while True:
+        try :
+            eve = heapq.heappop(events)
+        except IndexError :
+            break
+    
+        g_y = eve.y
         print(eve)
-    # end for
-    tycat(segments)
+    # end while
+    
+    print("End Bentley")
+    #tycat(segments)
     #TODO: merci de completer et de decommenter les lignes suivantes
     #results = lancer bentley ottmann sur les segments et l'ajusteur
     #...
@@ -25,7 +42,7 @@ def bentley(filename):
     #print("le nombre de coupes dans les segments (si un point d'intersection apparait dans
     # plusieurs segments, il compte plusieurs fois) est", ...)
 
-def init(segments):
+def init_events(segments):
     """
         Init events heap struct with start and end of each segments
     """
